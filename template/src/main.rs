2fab0c6a1947e501454bb344e1f8e5d32ac2b0be
Fredrik A. Madsen-Malmo <fredrik.malmo@icloud.com>
2022-12-05 13:13:30 +0000

    templatur

---
+++ template/src/main.rs
@@ -0,0 +1,32 @@
+pub fn part1(input_string: &str) -> i32 {
+    0
+}
+
+pub fn part2(input_string: &str) -> i32 {
+    0
+}
+
+fn main() {
+    let input = include_str!("input.txt");
+
+    println!("Part 1: {}", part1(input));
+
+    println!("Part 2: {}", part2(input));
+}
+
+#[cfg(test)]
+mod tests {
+    use crate::{part1, part2};
+
+    const TEST_INPUT: &str = "";
+
+    #[test]
+    fn test_part1() {
+        assert_eq!(part1(TEST_INPUT), 0);
+    }
+
+    #[test]
+    fn test_part2() {
+        assert_eq!(part2(TEST_INPUT), 0);
+    }
+}
