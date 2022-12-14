78fbc9520180549d79e8507b29a4b294610ebc2f
Fredrik A. Madsen-Malmo <fredrik.malmo@icloud.com>
2022-12-14 15:43:47 +0000

    fytti katta

--- day13/src/main.rs
+++ day13/src/main.rs
@@ -2,84 +2,88 @@ extern crate pest;
 #[macro_use]
 extern crate pest_derive;
 
+use std::cmp::Ordering;
+
 use pest::{iterators::Pair, Parser};
+
+#[derive(Debug, PartialEq, Eq)]
+enum Packet {
+    List(Vec<Packet>),
+    Signal(i32),
+}
 
 #[derive(Parser)]
 #[grammar = "grammar.pest"]
 pub struct InputParser;
 
-fn correctly_ordered_groups(list1: Pair<Rule>, list2: Pair<Rule>) -> Option<bool> {
-    let mut inner_list1 = list1.into_inner();
-    let mut inner_list2 = list2.into_inner();
+impl PartialOrd for Packet {
+    fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+        use Packet::{List, Signal};
 
-    loop {
-        let next_item_list1 = inner_list1.next();
-        let next_item_list2 = inner_list2.next();
-
-        if next_item_list1 == None && next_item_list2 == None {
-            return None;
-        }
-
-        if next_item_list1 == None {
-            return Some(true);
-        }
-
-        if next_item_list2 == None {
-            return Some(false);
-        }
-
-        let next_item_list1 = next_item_list1.unwrap();
-        let next_item_list2 = next_item_list2.unwrap();
-
-        match (next_item_list1.as_rule(), next_item_list2.as_rule()) {
-            (Rule::number, Rule::number) => {
-                let num1 = next_item_list1.as_str().parse::<i32>().unwrap();
-                let num2 = next_item_list2.as_str().parse::<i32>().unwrap();
-
-                if num1 == num2 {
-                    continue;
+        match (self, other) {
+            (Signal(sig1), Signal(sig2)) => {
+                if sig1 == sig2 {
+                    return None;
                 }
 
-                return Some(num1 < num2);
-            }
-            (Rule::list, Rule::list) => {
-                match correctly_ordered_groups(next_item_list1, next_item_list2) {
-                    None => continue,
-                    Some(result) => return Some(result),
+                if sig1 > sig2 {
+                    return Some(Ordering::Greater);
                 }
-            }
-            (Rule::number, Rule::list) => {
-                let num_str1 = next_item_list1.as_str();
-                let formated_array_string = format!("[{}]", num_str1);
-                let arrayified = InputParser::parse(Rule::list, formated_array_string.as_str())
-                    .unwrap()
-                    .next()
-                    .unwrap();
 
-                match correctly_ordered_groups(arrayified, next_item_list2) {
-                    None => continue,
-                    Some(result) => return Some(result),
-                }
+                return Some(Ordering::Less);
             }
-            (Rule::list, Rule::number) => {
-                let num_str2 = next_item_list2.as_str();
-                let formated_array_string = format!("[{}]", num_str2);
-                let arrayified = InputParser::parse(Rule::list, formated_array_string.as_str())
-                    .unwrap()
-                    .next()
-                    .unwrap();
+            (List(packets1), List(packets2)) => {
+                let mut items1 = packets1.clone().iter();
+                let mut items2 = packets2.clone().iter();
 
-                match correctly_ordered_groups(next_item_list1, arrayified) {
-                    None => continue,
-                    Some(result) => return Some(result),
+                loop {
+                    let next_item1 = items1.next();
+                    let next_item2 = items2.next();
+
+                    match (next_item1, next_item2) {
+                        (None, None) => return None,
+                        (Some(_), None) => return Some(Ordering::Greater),
+                        (None, Some(_)) => return Some(Ordering::Less),
+                        (Some(val1), Some(val2)) => match val1.partial_cmp(val2) {
+                            None => continue,
+                            value => return value,
+                        },
+                    }
                 }
             }
-            (_, _) => continue,
+            (list1, Packet::Signal(signal2)) => {
+                return list1.partial_cmp(&Packet::List(vec![Packet::Signal(*signal2)]))
+            }
+            (Packet::Signal(signal1), list2) => {
+                return Packet::List(vec![Packet::Signal(*signal1)]).partial_cmp(list2)
+            }
         }
     }
 }
 
-pub fn part1(input_string: &str) -> i32 {
+impl Ord for Packet {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.partial_cmp(other).unwrap()
+    }
+}
+
+fn list_to_packet(list: Pair<Rule>) -> Vec<Packet> {
+    let mut packets: Vec<Packet> = vec![];
+
+    let mut pairs_list = list.into_inner();
+
+    for element in pairs_list {
+        match element.as_rule() {
+            Rule::number => packets.push(Packet::Signal(element.as_str().parse::<i32>().unwrap())),
+            Rule::list => packets.push(Packet::List(list_to_packet(element))),
+            _ => unreachable!(),
+        }
+    }
+
+    packets
+}
+
+pub fn part2(input_string: &str) -> i32 {
     let parse = InputParser::parse(Rule::input, input_string)
         .unwrap()
         .next()
@@ -87,73 +91,69 @@ pub fn part1(input_string: &str) -> i32 {
 
     let mut count: i32 = 0;
 
-    for (index, group) in parse.into_inner().enumerate() {
-        match group.as_rule() {
-            Rule::group => {
-                let mut inner_group = group.into_inner();
+    let mut packets: Vec<Packet> = Vec::new();
 
-                let list1 = inner_group.next().unwrap();
-                let list2 = inner_group.next().unwrap();
-
-                if correctly_ordered_groups(list1, list2) == Some(true) {
-                    count += index as i32 + 1;
-                }
+    for (index, list) in parse.into_inner().enumerate() {
+        match list.as_rule() {
+            Rule::list => {
+                // println!("{:?}", list);
+                packets.push(Packet::List(list_to_packet(list)));
             }
             Rule::EOI => break,
             _ => unreachable!(),
         }
     }
 
-    count
-}
+    packets.sort();
 
-pub fn part2(input_string: &str) -> i32 {
-    0
+    let first_marker = packets
+        .iter()
+        .enumerate()
+        .find(|(_, val)| **val == Packet::List(vec![Packet::List(vec![Packet::Signal(2)])]))
+        .unwrap()
+        .0;
+
+    let second_marker = packets
+        .iter()
+        .enumerate()
+        .find(|(_, val)| **val == Packet::List(vec![Packet::List(vec![Packet::Signal(6)])]))
+        .unwrap()
+        .0;
+
+    return ((first_marker + 1) * (second_marker + 1)) as i32;
 }
 
 fn main() {
     let input = include_str!("input.txt");
-
-    println!("Part 1: {}", part1(input));
 
     println!("Part 2: {}", part2(input));
 }
 
 #[cfg(test)]
 mod tests {
-    use crate::{part1, part2};
+    use crate::part2;
 
     const TEST_INPUT: &str = "[1,1,3,1,1]
 [1,1,5,1,1]
-
 [[1],[2,3,4]]
 [[1],4]
-
 [9]
 [[8,7,6]]
-
 [[4,4],4,4]
 [[4,4],4,4,4]
-
 [7,7,7,7]
 [7,7,7]
-
 []
 [3]
-
 [[[]]]
 [[]]
-
 [1,[2,[3,[4,[5,6,7]]]],8,9]
-[1,[2,[3,[4,[5,6,0]]]],8,9]";
-
-    #[test]
-    fn test_part1() {
-        assert_eq!(part1(TEST_INPUT), 13);
-    }
+[1,[2,[3,[4,[5,6,0]]]],8,9]
+[[2]]
+[[6]]";
 
     #[test]
     fn test_part2() {
-        assert_eq!(part2(TEST_INPUT), 0);
+        assert_eq!(part2(TEST_INPUT), 140);
     }
 }
